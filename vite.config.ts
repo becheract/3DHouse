dda7edc7103d62f746f7b65a70e4be85ba2eb84d
Bechera Chapman-Tremblay <becherachapman@gmail.com>
2025-06-01 11:30:13 -0400

    testing something

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/3DHouse/' : ''
-})
+}
